dbabd0cf9a41246563ac897cdf19fc0eeaa8a9bd
Francis Aleke <ALEKEFRANCIS0@gmail.com>
2019-05-07 15:15:49 -0500

    added time travel to the previous moves in the game.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,26 +12,6 @@ function Square(props) {
   }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      squares:Array(9).fill(null),
-      xIsNext:true,
-    };
-  }
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-
   renderSquare(i) {
     return (
       <Square value={this.props.squares[i]}
@@ -41,13 +21,6 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
     return (
       <div>
         <div className="board-row">
